c7be5185c55efbd3c98ff16c92b94a617a62ac1d
ANJ-NISH <155297183+ANJ-NISH@users.noreply.github.com>
2024-11-23 18:13:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 function firstNonRepeatedChar(str) {
- // Write your code here
+
+	let arr=[];
+
+	for(let i=0;i<str.length;i++)
+		{
+			if(str.charAt(i) in arr)
+			{
+				return str.charAt(i);
+			}
+		}
+
+	return null;
 }
 const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
